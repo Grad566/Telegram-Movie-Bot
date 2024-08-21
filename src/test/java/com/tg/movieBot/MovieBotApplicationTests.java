abbe906077db68021b99d77b244dbcb4a0c7a520
Denis Orekhov <3008denis@gmail.com>
2024-08-21 16:00:19 +0300

    Create project structure

---
+++ src/test/java/com/tg/movieBot/MovieBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tg.movieBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
